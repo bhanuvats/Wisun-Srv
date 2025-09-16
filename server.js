89e66744656d9eb1ede04e530aec4c336b853c4d
PirdainBhalla <pirdain.bhalla@aaensa.com>
2025-09-16 17:36:01 +0530

    debugging timeout
    6 sec

--- server.js
+++ server.js
@@ -104,7 +104,7 @@ server.on("message", async (msg, rinfo) => {
             } catch (err) {
                 console.error("Error during cleanup:", err);
             }
-        }, 5000); // adjust timeout as per buffer batch frequency
+        }, 6000); // adjust timeout as per buffer batch frequency
 
     } catch (err) {
         console.error("Error handling packet:", err);
